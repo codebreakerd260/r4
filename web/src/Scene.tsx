8eb9129bfbd2342e13aa7a58a422658e53c6383d
BedadhamPranayTeja <pranay58483@gmail.com>
2026-01-16 11:23:19 +0530

    commit message

--- web/src/Scene.tsx
+++ web/src/Scene.tsx
@@ -7,10 +7,17 @@ export function Scene() {
     return (
         <Canvas camera={{ position: [-250, 150, -250], fov: 50 }} shadows>
             <color attach="background" args={['#202020']} />
+            <fog attach="fog" args={['#202020', 500, 2000]} />
 
             {/* Lights */}
             <ambientLight intensity={0.5} />
             <pointLight position={[500, 500, 500]} intensity={2.0} decay={0} castShadow />
+
+            {/* Ground */}
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.1, 0]} receiveShadow>
+                <planeGeometry args={[5000, 5000]} />
+                <meshStandardMaterial color="#202020" roughness={0.8} metalness={0.2} />
+            </mesh>
 
             {/* Robot Model */}
             <Suspense fallback={null}>
